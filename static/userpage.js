392d737f5c72f75f5ef484e2db773f0bc039d159
Bryan <btlinton99@gmail.com>
2019-12-26 07:59:56 -0800

    fixed whatever I did wrong in the ajax.js page

--- static/userpage.js
+++ static/userpage.js
@@ -1,59 +1,59 @@
 $(document).ready(function(){
-// delete button
-    $('#board').on('click','#delete_button', function(){
-        var del_id = $(this).attr('class');
-        var link ="/delete_idea/" + del_id
+    // delete button
+        $('#board').on('click','#delete_button', function(){
+            var del_id = $(this).attr('class');
+            var link ="/delete_idea/" + del_id
+            $.ajax({
+                url: link
+                // data: $("#idea").serialize()
+            })
+            .done(function(response){
+                $("#board").html(response)
+            })
+            return false
+        });
+    // submit idea
+        $('#idea').submit(function(){
+            $.ajax({
+                url: "/create_idea",
+                method: "POST",
+                data: $("#idea").serialize()
+            })
+            .done(function(response){
+                $("#board").html(response)
+                $('#idea_textarea').val('');
+            })
+            return false
+        });
+    // UnLike idea
+    $('#board').on('click','#unlike', function(){
+        var unlike_id = $(this).attr('class');
+        var link ="/unlike_idea/" + unlike_id
         $.ajax({
             url: link
-            // data: $("#idea").serialize()
         })
         .done(function(response){
             $("#board").html(response)
         })
         return false
     });
-// submit idea
-    $('#idea').submit(function(){
+    // Like idea
+    $('#board').on('click','#like', function(){
+        var like_id = $(this).attr('class');
+        var link ="/like_idea/" + like_id
         $.ajax({
-            url: "/create_idea",
-            method: "POST",
-            data: $("#idea").serialize()
+            url: link
         })
         .done(function(response){
             $("#board").html(response)
-            $('#idea_textarea').val('');
         })
         return false
     });
-// UnLike idea
-$('#board').on('click','#unlike', function(){
-    var unlike_id = $(this).attr('class');
-    var link ="/unlike_idea/" + unlike_id
-    $.ajax({
-        url: link
+    // edit button
+        $('#edit_idea').click(function(){
+            $("#idea{{idea.idea_id}}").html("<p>test<p>")
+        });
+        $('.testbutton').click(function(){
+            $(".testdiv").html("not working?")
+        });
     })
-    .done(function(response){
-        $("#board").html(response)
-    })
-    return false
-});
-// Like idea
-$('#board').on('click','#like', function(){
-    var like_id = $(this).attr('class');
-    var link ="/like_idea/" + like_id
-    $.ajax({
-        url: link
-    })
-    .done(function(response){
-        $("#board").html(response)
-    })
-    return false
-});
-// // edit button
-//     $('#edit_idea').click(function(){
-//         $("#idea{{idea.idea_id}}").html("<p>test<p>")
-//     });
-//     $('.testbutton').click(function(){
-//         $(".testdiv").html("not working?")
-//     });
-// })
